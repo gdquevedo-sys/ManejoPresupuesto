9a0feaa0a54ca6bd33dd2fdeaca5c94bf6f1225a
Gustavo Delgado <gdquevedo@gmail.com>
2025-07-01 22:52:04 -0600

    145. Vista de Actualizaciones de Transacciones

--- ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
+++ ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -1,12 +1,15 @@
 ﻿using Dapper;
 using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Data.SqlClient;
 
 namespace ManejoPresupuesto.Servicios
 {
     public interface IRepositorioTransacciones
     {
+        Task Actualizar(Transaccion transaccion, decimal montoAnterior, int cuentaAnterior);
         Task Crear(Transaccion transaccion);
+        Task<Transaccion> ObtenerPorId(int id, int usuarioId);
     }
     public class RepositorioTransacciones: IRepositorioTransacciones
     {
@@ -32,6 +35,36 @@ namespace ManejoPresupuesto.Servicios
                 commandType: System.Data.CommandType.StoredProcedure);
 
             transaccion.Id = id;
+        }
+
+        public async Task Actualizar(Transaccion transaccion, decimal montoAnterior,
+            int cuentaAnteriorId)
+        { 
+            using var connection = new SqlConnection (connectionString);
+            await connection.ExecuteAsync("Transacciones_Actualizar",
+                new
+                {
+                    transaccion.Id,
+                    transaccion.FechaTransaccion,
+                    transaccion.Monto,
+                    transaccion.CategoriaId,
+                    transaccion.CuentaId,
+                    transaccion.Nota,
+                    montoAnterior,
+                    cuentaAnteriorId
+                }, commandType: System.Data.CommandType.StoredProcedure);
+        }
+
+        public async Task<Transaccion> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<Transaccion>(
+                @"SELECT Transacciones.*, cat.TipoOperacionId
+                FROM Transacciones
+                INNER JOIN Categorias cat
+                ON cat.Id = Transacciones.CategoriaId
+                WHERE Transacciones.Id = @Id AND Transacciones.UsuarioId = @UsuarioId", 
+                new { id, usuarioId });
         }
     }
 }
