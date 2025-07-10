82cf66c95f74a42391c3b3b7a59c788cba782249
Gustavo Delgado <gdquevedo@gmail.com>
2025-07-10 12:55:06 -0600

    150. Vista de Movimientos de Cuentas y ScriptBD.sql

--- ManejoPresupuesto/Models/Transaccion.cs
+++ ManejoPresupuesto/Models/Transaccion.cs
@@ -13,12 +13,15 @@ namespace ManejoPresupuesto.Models
         [Range(1, maximum: int.MaxValue, ErrorMessage = "Debe seleccionar una categoría")]
         [Display(Name = "Categoría")]
         public int CategoriaId { get; set; }
-        [StringLength(maximumLength:1000, ErrorMessage = "La nota no puede pasar de {1} caracteres" )]
+        [StringLength(maximumLength: 1000, ErrorMessage = "La nota no puede pasar de {1} caracteres")]
         public string Nota { get; set; }
         [Range(1, maximum: int.MaxValue, ErrorMessage = "Debe seleccionar una cuenta")]
-        [Display(Name ="Cuenta")]
+        [Display(Name = "Cuenta")]
         public int CuentaId { get; set; }
         [Display(Name = "Tipo Operación")]
         public TipoOperacion TipoOperacionId { get; set; } = TipoOperacion.Ingreso;
+
+        public string Cuenta { get; set; }
+        public string Categoria { get; set; }
     }
 }
--- ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
+++ ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -10,6 +10,7 @@ namespace ManejoPresupuesto.Servicios
         Task Actualizar(Transaccion transaccion, decimal montoAnterior, int cuentaAnterior);
         Task Borrar(int id);
         Task Crear(Transaccion transaccion);
+        Task<IEnumerable<Transaccion>> ObtenerPorCuentaId(ObtenerTransaccionesPorCuenta modelo);
         Task<Transaccion> ObtenerPorId(int id, int usuarioId);
     }
     public class RepositorioTransacciones: IRepositorioTransacciones
@@ -36,6 +37,22 @@ namespace ManejoPresupuesto.Servicios
                 commandType: System.Data.CommandType.StoredProcedure);
 
             transaccion.Id = id;
+        }
+
+        public async Task<IEnumerable<Transaccion>> ObtenerPorCuentaId(
+            ObtenerTransaccionesPorCuenta modelo)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<Transaccion>
+                (@"SELECT t.Id, t.Monto, t.FechaTransaccion, c.Nombre as Categoria,
+                    cu.Nombre as Cuenta, c.TipoOperacionId
+                    FROM Transacciones t
+                    INNER JOIN Categorias c
+                    ON c.Id = t.CategoriaId
+                    INNER JOIN Cuentas cu
+                    ON cu.Id = t.CuentaId
+                    WHERE t.CuentaId = @CuentaId AND t.UsuarioId = @UsuarioId                    
+                    AND FechaTransaccion BETWEEN @FechaInicio AND @FechaFin", modelo); //AND FechaTransaccion BETWEEN @FechaInicio AND @FechaFin
         }
 
         public async Task Actualizar(Transaccion transaccion, decimal montoAnterior,
