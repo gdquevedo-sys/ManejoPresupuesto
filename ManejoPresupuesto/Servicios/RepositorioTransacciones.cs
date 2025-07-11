13c4d48f4fcd6cd3b28c9e3c80a8df514f92a5f8
Gustavo Delgado <gdquevedo@gmail.com>
2025-07-11 16:24:01 -0600

    156.Reporte Semanal - Query - Group By con DateDiff, 157.Reporte Semanal - Algoritmo, 158.Reporte Semanal - Vista

--- ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
+++ ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -12,6 +12,7 @@ namespace ManejoPresupuesto.Servicios
         Task Crear(Transaccion transaccion);
         Task<IEnumerable<Transaccion>> ObtenerPorCuentaId(ObtenerTransaccionesPorCuenta modelo);
         Task<Transaccion> ObtenerPorId(int id, int usuarioId);
+        Task<IEnumerable<ResultadoObtenerPorSemana>> ObtenerPorSemana(ParametroObtenerTransaccionesPorUsuario modelo);
         Task<IEnumerable<Transaccion>> ObtenerPorUsuarioId(ParametroObtenerTransaccionesPorUsuario modelo);
     }
     public class RepositorioTransacciones: IRepositorioTransacciones
@@ -101,6 +102,22 @@ namespace ManejoPresupuesto.Servicios
                 ON cat.Id = Transacciones.CategoriaId
                 WHERE Transacciones.Id = @Id AND Transacciones.UsuarioId = @UsuarioId", 
                 new { id, usuarioId });
+        }
+
+        public async Task<IEnumerable<ResultadoObtenerPorSemana>> ObtenerPorSemana(
+            ParametroObtenerTransaccionesPorUsuario modelo) 
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<ResultadoObtenerPorSemana>(@"
+                    Select DATEDIFF(d, @fechaInicio, FechaTransaccion) / 7 + 1 as Semana,
+                    SUM(Monto) as Monto, cat.TipoOperacionId
+                    FROM Transacciones
+                    INNER JOIN Categorias cat
+                    ON cat.Id = Transacciones.CategoriaId
+                    WHERE Transacciones.UsuarioId = @usuarioId AND
+                    FechaTransaccion BETWEEN @fechaInicio and @fechaFin
+                    GROUP BY DATEDIFF(d, @fechaInicio, FechaTransaccion) / 7, cat.TipoOperacionId
+                    ", modelo);
         }
 
         public async Task Borrar(int id)
